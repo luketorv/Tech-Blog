aefd5068311e1384547cc9c2e49e51b2a101ef4c
luketorv <104324965+luketorv@users.noreply.github.com>
2022-08-28 15:40:39 -0500

    added controllers and handlebars

---
+++ controllers/home-routes.js
@@ -0,0 +1,37 @@
+// import router and models
+const router = require("express").Router();
+const { Post, Comment, User } = require("../models/");
+ 
+router.get("/", (req, res) => {
+ //for the homepage root path, find all Posts
+ Post.findAll({
+   include: [User],
+ })
+   .then((dbPostData) => {
+     const posts = dbPostData.map((post) => post.get({ plain: true }));
+	// pass in all posts data into the ‘all-posts’ template
+     res.render("all-posts", { posts });
+   })
+   .catch((err) => {
+     res.status(500).json(err);
+   });
+});
+
+router.get("/post/:id", (req, res) => {
+    //follow a similar pattern to the homepage route. 
+    //get the data using the id in params, then pass it into the ‘single-post’ view
+   Post.findByPk(req.params.id, { })  
+   res.render("single-post", { post });
+  })
+  
+  router.get("/login", (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect("/");
+      return;
+    }
+    
+    res.render("login");
+   });
+
+   module.exports = router;
+   
--- server.js
+++ server.js
@@ -2,14 +2,21 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
 const sequelize = require("./config/config");
+const models = require("./models");
+
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.use(require('./controllers/'));
 
 app.listen(PORT, async () => {
-    console.log(`App listening on port ${PORT}!`);
-    try {
-        await sequelize.authenticate() // checks that the connection works
-        console.log('connected to db!')
-    } catch(e){
-        console.log(e)
-    }
-  });
- 
+   console.log(`App listening on port ${PORT}!`);
+   try {
+    await sequelize.sync({ force: true })
+    console.log('connected to db!')
+} catch(e){
+    console.log(e)
+}
+});
+
